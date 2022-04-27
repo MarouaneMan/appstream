2d908f5c145bf2e92bd618802ee0db5ace06e228
Marouane <manaa.marouane@gmail.com>
2022-04-27 23:58:48 +0200

    refactor(app): commented unused session middlewares

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { QueryFailedErrorExceptionFilter } from './common/filters';
 import helmet from 'helmet';
-import { ConfigService } from '@nestjs/config';
-import session from 'express-session';
-import passport from 'passport';
+// import { ConfigService } from '@nestjs/config';
+// import session from 'express-session';
+// import passport from 'passport';
 
 async function bootstrap() {
 
@@ -28,16 +28,16 @@ async function bootstrap() {
 
   app.useGlobalFilters(new QueryFailedErrorExceptionFilter());
 
-  const configService = app.get(ConfigService);
-  app.use(
-    session({
-      secret: configService.get('jwt').secret,
-      resave: false,
-      saveUninitialized: false,
-    }),
-  );
-  app.use(passport.initialize());
-  app.use(passport.session());
+  // const configService = app.get(ConfigService);
+  // app.use(
+  //   session({
+  //     secret: configService.get('jwt').secret,
+  //     resave: false,
+  //     saveUninitialized: false,
+  //   }),
+  // );
+  // app.use(passport.initialize());
+  // app.use(passport.session());
 
   await app.listen(3000);
 }
